a49b50a3202bffc45fa0476ac50d74c095173ced
Samer Saied <samersaied@Samers-MacBook-Pro.local>
2024-02-18 08:45:18 +0200

    add animation

--- app/today/page.jsx
+++ app/today/page.jsx
@@ -8,7 +8,7 @@ import Navbar from "@/components/common/Navbar";
 import { useEffect, useState } from "react";
 import PathWidget from "@/components/common/path_widget";
 import { GetTodayDealProducts } from "@/repository/products_repository";
-import { SpeedSaleCardWidget } from "@/components/homepage/speed_sale/speed_cards_widget";
+import { TodayDealCardWidget } from "@/components/homepage/deal_today/today_deal_card_widget";
 import LoadingPage from "@/components/common/loading";
 import NoItemsWidget from "@/components/common/no_items_widget";
 
@@ -43,7 +43,7 @@ export default function TodayOffersPage() {
       {!loading && products.length > 0 && (
         <div className=" grid lg:grid-cols-3 md:grid-cols-2 grid-cols-2 gap-2 md:w-9/12 px-2 mx-auto">
           {products.map((product) => (
-            <SpeedSaleCardWidget key={product.id} product={product} />
+            <TodayDealCardWidget key={product.id} product={product} />
           ))}
         </div>
       )}
